033b9b5ac3a1827a500591c0d2afddc314602d98
Zixuan Yin <zixuan@orthanc.ece.mcgill.ca>
2018-04-07 16:12:24 -0400

    yyuhoooo

--- testing.py
+++ testing.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import numpy as np
 import pickle as pkl
@@ -12,8 +13,33 @@ import structured_forests
 from structured_forests import StructuredRandomForrest, BSDS500
 
 bsds = BSDS500(dirpath='./BSR')
+
+sobel = filters.sobel
+scharr = filters.scharr
+prewitt = filters.prewitt
+
+img = bsds.read_image('train/236017')
+img = color.rgb2gray(img)
+sobel_result = sobel(img)
+scharr_result = scharr(img)
+prewitt_result = prewitt(img)
+
+for _str,result in [('sobel',sobel_result),('scharr',scharr_result),('prewitt',prewitt_result)]:
+    plt.imshow(sobel_result,cmap='Greys')
+    plt.title(_str+' result')
+    plt.savefig('./figs/'+_str+'_result.png', dpi=400, bbox_inches='tight')
+
 srf = structured_forests.load_model('./results/Sat_Apr_7_2018/Sat_Apr_7_2018_v1.pkl')
-srf.threshold = 0.1
+edge_map = srf.predict_edge_map(bsds.read_image('train/236017'),
+                    groundTruth=bsds.get_edge_map('train/236017')[0],
+                    imshow=False)
+                    #  imsave=True,
+                    #  fn=bsds.test_ids[i]+'_floating_threshold')
+plt.imshow(edge_map,cmap='Greys')
+plt.title('StructuredRandomForrest result')
+plt.savefig('./figs/srf_result.png', dpi=400, bbox_inches='tight')
+
+sys.exit()
 for i in range(10):
     start = time.time()
     edge_map = srf.predict_edge_map(bsds.read_image(bsds.test_ids[i]),
