737ea3e53e2ffa47d9111e56cab8c4797d3b3c62
alex-yin <zixuan.yin@mail.mcgill.ca>
2018-04-07 21:05:48 -0400

    more scripting

--- testing.py
+++ testing.py
@@ -6,7 +6,7 @@ import pickle as pkl
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.decomposition import PCA
-from skimage import io, filters, color, morphology
+from skimage import io, filters, color, morphology, feature
 from scipy.io import loadmat
 
 import structured_forests
@@ -17,18 +17,21 @@ bsds = BSDS500(dirpath='./BSR')
 sobel = filters.sobel
 scharr = filters.scharr
 prewitt = filters.prewitt
+canny = feature.canny
 
-img = bsds.read_image('train/236017')
+img = bsds.read_image('train/247085')
 img = color.rgb2gray(img)
 sobel_result = sobel(img)
 scharr_result = scharr(img)
 prewitt_result = prewitt(img)
+canny_result = canny(img)
 
-for _str,result in [('sobel',sobel_result),('scharr',scharr_result),('prewitt',prewitt_result)]:
+for _str,result in [('sobel',sobel_result),('scharr',scharr_result),('prewitt',prewitt_result),('canny',canny_result)]:
     plt.imshow(sobel_result,cmap='Greys')
     plt.title(_str+' result')
-    plt.savefig('./figs/'+_str+'_result.png', dpi=400, bbox_inches='tight')
+    plt.show()
 
+sys.exit()
 srf = structured_forests.load_model('./results/Sat_Apr_7_2018/Sat_Apr_7_2018_v1.pkl')
 edge_map = srf.predict_edge_map(bsds.read_image('train/236017'),
                     groundTruth=bsds.get_edge_map('train/236017')[0],
